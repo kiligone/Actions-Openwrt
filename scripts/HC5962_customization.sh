f5b960f13640fa9d9f9bebbc0718a023584e0461
kiligone <ow4867208@gmail.com>
2022-04-22 14:15:05 +0800

    v22.03.0-rc1

--- scripts/HC5962_customization.sh
+++ scripts/HC5962_customization.sh
@@ -3,6 +3,6 @@
 # mbedtls
 cp -v ../patches/201-Camellia-block-cipher.patch package/libs/mbedtls/patches/
 # mkhash
-# sed -i 's/mkhash md5/echo ecc7515846d9a85938da6124aee98749/g' include/kernel-defaults.mk
+sed -i 's/$(MKHASH) md5/echo 65fb974d16c7195fa4353fc4a101041e/g' include/kernel-defaults.mk
 
 exit 0
