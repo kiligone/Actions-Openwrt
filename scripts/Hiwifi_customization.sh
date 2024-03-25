24eee3fbb1dcff49d16cd10ff5ad183fd8dd6d98
kiligone <102161823+kiligone@users.noreply.github.com>
2024-03-25 17:51:30 +0800

    Update Hiwifi_customization.sh

--- scripts/Hiwifi_customization.sh
+++ scripts/Hiwifi_customization.sh
@@ -3,6 +3,6 @@
 # mbedtls
 cp -v ../patches/201-Camellia-block-cipher.patch package/libs/mbedtls/patches/
 # mkhash
-sed -i 's/mkhash md5/echo bee655e7f41679981b85a223f3aba35b/g' include/kernel-defaults.mk
+sed -i 's/mkhash md5/echo 95238bfd570012d05891b8156dec4a28/g' include/kernel-defaults.mk
 
 exit 0
