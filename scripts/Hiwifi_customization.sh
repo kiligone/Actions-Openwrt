7c0178e8ef1c1c17b6bbc7383f4e7442912e8465
kiligone <102161823+kiligone@users.noreply.github.com>
2024-03-25 17:52:03 +0800

    Update Hiwifi_customization.sh

--- scripts/Hiwifi_customization.sh
+++ scripts/Hiwifi_customization.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-sed -i 's/$(MKHASH) md5/echo 9c242f353867f49a96054ff8c9f2c460/g' include/kernel-defaults.mk
+sed -i 's/$(MKHASH) md5/echo bee655e7f41679981b85a223f3aba35b/g' include/kernel-defaults.mk
 
 exit 0
