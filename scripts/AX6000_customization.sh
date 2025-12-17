7269da929b39a088586b4f410943306c31e7d620
kiligone <102161823+kiligone@users.noreply.github.com>
2025-12-17 14:19:08 +0800

    Update AX6000_customization.sh

--- scripts/AX6000_customization.sh
+++ scripts/AX6000_customization.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-cp -f ../patches/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch target/linux/generic/hack-5.15/
-sed -i 's/$(MKHASH) md5/echo 83850846aeb31a278a010941bbebf326/g' include/kernel-defaults.mk
+cp -f ../patches/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch target/linux/generic/hack-6.6/
+sed -i 's/$(MKHASH) md5/echo 6a9e125268c43e0bae8cecb014c8ab03/g' include/kernel-defaults.mk
 
 exit 0
